b724e37407688558f832e8fef3c3294fec520049
madhu11266 <sarimella123madhu@gmail.com>
2025-10-01 22:59:55 +0530

    first commit

---
+++ src/main/java/com/example/awspursuitdemo/AwspursuitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.awspursuitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspursuitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspursuitdemoApplication.class, args);
+	}
+
+}
