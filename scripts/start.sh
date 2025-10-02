109199a992e80b9d7f6670c84cfeb6adfc84e608
Madhu11266 <86288692+Madhu11266@users.noreply.github.com>
2025-10-02 17:59:28 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,8 +9,13 @@ DB2_URL=$(aws ssm get-parameter --name "/springboot/DB2_URL" --with-decryption -
 DB2_USERNAME=$(aws ssm get-parameter --name "/springboot/DB2_USERNAME" --with-decryption --query "Parameter.Value" --output text)
 DB2_PASSWORD=$(aws ssm get-parameter --name "/springboot/DB2_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
 
-# Run pursuit.jar with secrets
+# Ensure log directory exists
+mkdir -p /home/ec2-user/app/logs
+chmod 755 /home/ec2-user/app/logs
+
+# Run pursuit.jar with secrets and redirect logs
 nohup java -jar pursuit.jar \
-  --spring.datasource.url=$DB2_URL \
-  --spring.datasource.username=$DB2_USERNAME \
-  --spring.datasource.password=$DB2_PASSWORD > app.log 2>&1 &
+  --spring.datasource.url="$DB2_URL" \
+  --spring.datasource.username="$DB2_USERNAME" \
+  --spring.datasource.password="$DB2_PASSWORD" \
+  > /home/ec2-user/app/logs/app.log 2>&1 &
